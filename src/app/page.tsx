234c30a4dd829b016dc5c4d539ab514b165ec776
Anže Fric <fanze@HRCDOO.LOCAL>
2025-08-28 11:02:12 +0200

    Added ContainerWrapper to include NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,24 @@
 "use client";
 
-import { useEffect, useRef, useCallback } from "react";
-import NavSection from "@/components/global/nav/NavSection";
 import BannerSection from "@/components/home/BannerSection";
-import styles from "@/styles/app/styles.module.css";
 import ReferenceSection from "@/components/home/ReferenceSection";
+import ContainerWrapper from "@/components/global/wrappers/ContainerWrapper";
 import { useWindowSize } from "@/contexts/WindowSizeContext";
 
 export default function Home() {
-  const { height, deviceType } = useWindowSize();
-
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-  const pageNum = useRef(0);
-
-  const TOTAL_PAGES = 3; // Number of sections
-
-  const scrollDown = (isMobile: boolean) => {
-    if (!scrollContainerRef.current) return;
-    if (pageNum.current >= TOTAL_PAGES - 1) return;
-
-    if (isMobile) {
-      window.scrollBy({
-        top: height - window.scrollY,
-        behavior: "smooth",
-      });
-    } else {
-      pageNum.current += 1;
-      scrollContainerRef.current.style.transform = `translate3d(0px, -${
-        height * pageNum.current
-      }px, 0px)`;
-    }
-  };
-
-  const scrollUp = () => {
-    if (!scrollContainerRef.current) return;
-    if (pageNum.current <= 0) return;
-
-    pageNum.current -= 1;
-    scrollContainerRef.current.style.transform = `translate3d(0px, -${
-      height * pageNum.current
-    }px, 0px)`;
-  };
-
-  const scroll = useCallback(
-    (e: WheelEvent) => {
-      if (deviceType === "desktop") {
-        e.preventDefault(); // Without it scrolls past the start of section
-        e.deltaY > 0 ? scrollDown(false) : scrollUp();
-      }
-    },
-    [deviceType, scrollDown, scrollUp]
-  );
-
-  useEffect(() => {
-    if (deviceType === "desktop") {
-      document.addEventListener("wheel", scroll, { passive: false });
-
-      return () => {
-        document.removeEventListener("wheel", scroll);
-      };
-    }
-  }, [deviceType, scroll]);
+  const { deviceType } = useWindowSize();
 
   return (
-    <div
-      ref={scrollContainerRef}
-      className={
-        deviceType === "desktop" ? styles.container : styles.mobileContainer
-      }
-    >
-      <NavSection />
-      <BannerSection scrollDown={() => scrollDown(deviceType !== "desktop")} />
-      <ReferenceSection />
-      <ReferenceSection />
-    </div>
+    <ContainerWrapper sections={3}>
+      {({ scrollUp, scrollDown }) => (
+        <>
+          <BannerSection
+            scrollDown={() => scrollDown(deviceType !== "desktop")}
+          />
+          <ReferenceSection />
+          <ReferenceSection />
+        </>
+      )}
+    </ContainerWrapper>
   );
 }
---
+++ src/components/global/wrappers/ContainerWrapper.tsx
@@ -0,0 +1,85 @@
+import styles from "@/styles/components/global/container-styles.module.css";
+import { useWindowSize } from "@/contexts/WindowSizeContext";
+import { useCallback, useEffect, useRef } from "react";
+import NavSection from "@/components/global/nav/NavSection";
+
+interface Props {
+  sections: number; // Num of sections in the children
+  children: (args: {
+    scrollUp: () => void;
+    scrollDown: (isMobile: boolean) => void;
+  }) => React.ReactNode;
+}
+
+export default function ContainerWrapper({
+  sections,
+  children,
+}: Readonly<Props>) {
+  const { height, deviceType } = useWindowSize();
+
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const pageNum = useRef(0);
+
+  const scrollDown = (isMobile: boolean) => {
+    if (!scrollContainerRef.current) return;
+    if (pageNum.current >= sections - 1) return;
+
+    if (isMobile) {
+      window.scrollBy({
+        top: height - window.scrollY,
+        behavior: "smooth",
+      });
+    } else {
+      pageNum.current += 1;
+      scrollContainerRef.current.style.transform = `translate3d(0px, -${
+        height * pageNum.current
+      }px, 0px)`;
+    }
+  };
+
+  const scrollUp = () => {
+    if (!scrollContainerRef.current) return;
+    if (pageNum.current <= 0) return;
+
+    pageNum.current -= 1;
+    scrollContainerRef.current.style.transform = `translate3d(0px, -${
+      height * pageNum.current
+    }px, 0px)`;
+  };
+
+  const scroll = useCallback(
+    (e: WheelEvent) => {
+      if (deviceType === "desktop") {
+        e.preventDefault(); // Without it scrolls past the start of section
+        e.deltaY > 0 ? scrollDown(false) : scrollUp();
+      }
+    },
+    [deviceType, scrollDown, scrollUp]
+  );
+
+  useEffect(() => {
+    if (deviceType === "desktop") {
+      document.addEventListener("wheel", scroll, { passive: false });
+
+      return () => {
+        document.removeEventListener("wheel", scroll);
+      };
+    }
+  }, [deviceType, scroll]);
+
+  return (
+    <div
+      ref={scrollContainerRef}
+      className={
+        deviceType === "desktop" ? styles.container : styles.mobileContainer
+      }
+    >
+      <NavSection />
+
+      {children({
+        scrollUp,
+        scrollDown,
+      })}
+    </div>
+  );
+}
