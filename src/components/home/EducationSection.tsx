1accc8589f36aa01b44d817e63676d26adbcd39e
Anže Fric <fanze@HRCDOO.LOCAL>
2025-09-03 12:20:13 +0200

    Added section title

--- src/components/home/EducationSection.tsx
+++ src/components/home/EducationSection.tsx
@@ -7,16 +7,17 @@ interface Props {
 export default function EducationSection({ focused }: Readonly<Props>) {
   return (
     <div className={styles.educationContainer}>
+      <h2 className={`${styles.sectionTitle} titleText`}>Education</h2>
       <div className={styles.contentContainer}>
         <div
           className={`${styles.bachelorSchool} ${
             focused ? styles.animateLeft : ""
           }`}
         >
-          <p className={styles.title}>Bachelor</p>
-          <p className={styles.subtitle}>Computer Science</p>
-          <p className={styles.school}>UM FERI</p>
-          <p className={styles.year}>2021 - 2025</p>
+          <p className={`${styles.title} primaryText`}>Bachelor</p>
+          <p className={`${styles.subtitle} primaryText`}>Computer Science</p>
+          <p className={`${styles.school} primaryText`}>UM FERI</p>
+          <p className={`${styles.year} primaryText`}>2021 - 2025</p>
         </div>
         <div className={styles.line} />
         <div
@@ -24,14 +25,16 @@ export default function EducationSection({ focused }: Readonly<Props>) {
             focused ? styles.animateRight : ""
           }`}
         >
-          <p className={styles.title}>Secondary School</p>
-          <p className={styles.subtitle}>Computer Science</p>
-          <p className={styles.school}>Srednja sola Ravne na Koroskem</p>
-          <p className={styles.year}>2017 - 2021</p>
+          <p className={`${styles.title} primaryText`}>Secondary School</p>
+          <p className={`${styles.subtitle} primaryText`}>Computer Science</p>
+          <p className={`${styles.school} primaryText`}>
+            Srednja sola Ravne na Koroskem
+          </p>
+          <p className={`${styles.year} primaryText`}>2017 - 2021</p>
         </div>
       </div>
       <div className={styles.primarySchool}>
-        <p className={styles.title}>Primary School</p>
+        <p className={`${styles.title} primaryText`}>Primary School</p>
       </div>
     </div>
   );
