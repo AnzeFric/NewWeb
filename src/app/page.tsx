a87fba654a566df7b7f947c4c82894b1ca1ee03b
Anže Fric <fanze@HRCDOO.LOCAL>
2025-09-05 10:32:56 +0200

    Added background slide animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,54 @@ import ContainerWrapper from "@/components/global/wrappers/ContainerWrapper";
 import { useWindowSize } from "@/contexts/WindowSizeContext";
 import { useEffect, useState } from "react";
 import eduStyles from "@/constants/styles/components/home/education-styles.module.css";
+import workStyles from "@/constants/styles/components/home/work-styles.module.css";
 import WorkSection from "@/components/home/WorkSection";
 
-// Sections: Education, Work history, Projects, Hobbies
+// Sections to add: Hobbies
 export default function Home() {
   const { deviceType } = useWindowSize();
   const [educationFocused, setEducationFocused] = useState(false);
+  const [workFocused, setWorkFocused] = useState(false);
 
   useEffect(() => {
     const eduSection = document.querySelector(
       `.${eduStyles.educationContainer}`
     );
+    const workSection = document.querySelector(`.${workStyles.workContainer}`);
+
+    let eduObserver: IntersectionObserver | null = null;
+    let workObserver: IntersectionObserver | null = null;
 
     if (eduSection) {
-      const observer = new IntersectionObserver(
+      eduObserver = new IntersectionObserver(
         ([entry]) => {
           if (entry.isIntersecting) {
             setEducationFocused(true);
-            observer.disconnect();
+            eduObserver?.disconnect();
           }
         },
         { threshold: 0.3 }
       );
-
-      observer.observe(eduSection);
-
-      return () => {
-        observer.disconnect();
-      };
+      eduObserver.observe(eduSection);
     }
+
+    if (workSection) {
+      workObserver = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setWorkFocused(true);
+            workObserver?.disconnect();
+          }
+        },
+        { threshold: 0.3 }
+      );
+      workObserver.observe(workSection);
+    }
+
+    return () => {
+      eduObserver?.disconnect();
+      workObserver?.disconnect();
+    };
   }, []);
 
   // Scrolls to top after page reload
@@ -50,7 +69,7 @@ export default function Home() {
             scrollDown={() => scrollDown(deviceType !== "desktop")}
           />
           <EducationSection focused={educationFocused} />
-          <WorkSection />
+          <WorkSection focused={workFocused} />
         </>
       )}
     </ContainerWrapper>
--- src/components/home/WorkSection.tsx
+++ src/components/home/WorkSection.tsx
@@ -1,9 +1,18 @@
 import styles from "@/constants/styles/components/home/work-styles.module.css";
 
-export default function WorkSection() {
+interface Props {
+  focused: boolean;
+}
+
+export default function WorkSection({ focused }: Readonly<Props>) {
   return (
     <div className={styles.workContainer}>
       <h2 className={`${styles.sectionTitle} titleText`}>Work history</h2>
+      <div
+        className={`${styles.contentContainer} ${
+          focused ? styles.animateRight : styles.offscreenRight
+        }`}
+      />
     </div>
   );
 }
