3d239043f4eb0b41e40e68cb87783e67296092f6
Anže Fric <fanze@HRCDOO.LOCAL>
2025-09-05 12:01:10 +0200

    Added demo carousel

--- src/components/home/WorkSection.tsx
+++ src/components/home/WorkSection.tsx
@@ -1,10 +1,18 @@
 import styles from "@/constants/styles/components/home/work-styles.module.css";
+import { useWindowSize } from "@/contexts/WindowSizeContext";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
 
 interface Props {
   focused: boolean;
 }
 
 export default function WorkSection({ focused }: Readonly<Props>) {
+  const { deviceType } = useWindowSize();
+
+  const direction = deviceType === "desktop" ? "horizontal" : "vertical";
+
   return (
     <div className={styles.workContainer}>
       <h2 className={`${styles.sectionTitle} titleText`}>Work history</h2>
@@ -12,7 +20,38 @@ export default function WorkSection({ focused }: Readonly<Props>) {
         className={`${styles.contentContainer} ${
           focused ? styles.animateRight : styles.offscreenRight
         }`}
-      />
+      >
+        <Swiper
+          direction={direction}
+          slidesPerView={2}
+          spaceBetween={50}
+          pagination={false}
+          autoplay={{ delay: 10000, disableOnInteraction: false }}
+          speed={800}
+          modules={[Autoplay]}
+          style={{ height: direction === "vertical" ? "200px" : "auto" }}
+          loop={true}
+        >
+          <SwiperSlide>
+            <div>
+              <h3>Work 1</h3>
+              <p>Role / Company</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div>
+              <h3>Work 2</h3>
+              <p>Role / Company</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div>
+              <h3>Work 3</h3>
+              <p>Role / Company</p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
