6e3c7a5349fff1d7f53c4bb0bffbb87ccd161973
Anže Fric <fanze@HRCDOO.LOCAL>
2025-09-09 15:32:48 +0200

    Removed component and simplified desing

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import ContainerWrapper from "@/components/global/wrappers/ContainerWrapper";
-import CvListDisplay from "@/components/cv/CvListDisplay";
+import styles from "@/constants/styles/components/cv/list-styles.module.css";
+import { useState } from "react";
+import { CVData } from "@/constants/interfaces/cv";
+
+// MAX 21 CVS
 
 export default function CV() {
+  const [cvList, setCvList] = useState<Array<CVData>>([]); // To zustand store
+
   return (
     <ContainerWrapper sections={1}>
-      {({ scrollDown }) => <CvListDisplay />}
+      {({ scrollDown }) => (
+        <div className={styles.container}>
+          <h2 className={`${styles.title} titleText`}>CV LIST</h2>
+          <div className={styles.listContainer}>
+            {cvList.map((cv, index) => (
+              <div className={styles.itemContainer} key={index + cv.uuid}>
+                <p>{cv.name}</p>
+              </div>
+            ))}
+            {cvList.length < 21 && (
+              <div className={styles.addContainer}>
+                <p className={`${styles.addText} titleText`}>Add new +</p>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </ContainerWrapper>
   );
 }
