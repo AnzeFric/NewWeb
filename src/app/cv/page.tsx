6f0670b1a6f0efbe78e04c0ce993f6a5100c0511
Anže Fric <fanze@HRCDOO.LOCAL>
2025-09-10 08:16:47 +0200

    Made cv page more responsive and fixed nav bar not displaying correct UI between 1200 and 1280px

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -5,27 +5,36 @@ import styles from "@/constants/styles/components/cv/list-styles.module.css";
 import { useState } from "react";
 import { CVData } from "@/constants/interfaces/cv";
 
-// MAX 21 CVS
+// MAX 9 CVS
 
 export default function CV() {
-  const [cvList, setCvList] = useState<Array<CVData>>([]); // To zustand store
+  const [cvList, setCvList] = useState<Array<CVData>>([
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+    { name: "", uuid: "", date: new Date() },
+  ]); // To zustand store
 
   return (
     <ContainerWrapper sections={1}>
       {() => (
-        <div className={styles.container}>
+        <div className={styles.cvContainer}>
           <h2 className={`${styles.title} titleText`}>CV LIST</h2>
-          <div className={styles.listContainer}>
-            {cvList.map((cv, index) => (
-              <div className={styles.itemContainer} key={index + cv.uuid}>
-                <p>{cv.name}</p>
-              </div>
-            ))}
-            {cvList.length < 21 && (
-              <div className={styles.addContainer}>
-                <p className={`${styles.addText} titleText`}>Add new +</p>
-              </div>
-            )}
+          <div className={styles.contentContainer}>
+            <div className={styles.listContainer}>
+              {cvList.map((cv, index) => (
+                <div className={styles.itemContainer} key={index + cv.uuid} />
+              ))}
+              {cvList.length < 9 && (
+                <div className={styles.addContainer}>
+                  <p className={`${styles.addText} titleText`}>Add new +</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       )}
